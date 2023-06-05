bb4750f2400ac99c7ef63254a0892c1624e5ddb8
Atharve Pandey <pandey.atharve@gmail.com>
2023-06-05 17:59:24 -0400

    used prettier to format the document

--- Chapter1Excercises/C code/ThirdExcersize.c
+++ Chapter1Excercises/C code/ThirdExcersize.c
@@ -7,20 +7,20 @@ int main(int argc, char const *argv[])
         you should print cel values if far values are 0, 20, ... 300
     */
 
-    float far, cel, lower, upper, step; //we can have var declarations in one line
+    float far, cel, lower, upper, step; // we can have var declarations in one line
 
     lower = 0;
     upper = 300;
     step = 20;
 
-    printf("farValue: \t celValue: \n"); // \t indicates a tab (4 spaces) and \n makes  a new line since printf can't make one..
-    printf("--------------------------\n"); //print as a table like the specifications on page 16 of the book
+    printf("farValue: \t celValue: \n");    // \t indicates a tab (4 spaces) and \n makes  a new line since printf can't make one..
+    printf("--------------------------\n"); // print as a table like the specifications on page 16 of the book
 
     while (far <= upper)
     {
-        cel = (5.0 / 9.0) * (far - 32.0); //since we're using floats, all calculations will return a float (i.e int + float = float, the int will convert into float...)
-        printf("%3.0f \t | \t %6.3f \n", far, cel); //the number 3.0 means 3 characters wide, and 0 digits after the decimal; 6.3 -> 6 char wide, and 3 digits after decimal
-        far = far + step; //update
+        cel = (5.0 / 9.0) * (far - 32.0);           // since we're using floats, all calculations will return a float (i.e int + float = float, the int will convert into float...)
+        printf("%3.0f \t | \t %6.3f \n", far, cel); // the number 3.0 means 3 characters wide, and 0 digits after the decimal; 6.3 -> 6 char wide, and 3 digits after decimal
+        far = far + step;                           // update
     }
 
     return 0;
