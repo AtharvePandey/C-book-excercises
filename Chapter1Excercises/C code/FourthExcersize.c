9597b75d2622e8a6121b70af3854900a19b99024
Atharve Pandey <pandey.atharve@gmail.com>
2023-06-05 18:02:23 -0400

    the backbone for the fourth excersize

---
+++ Chapter1Excercises/C code/FourthExcersize.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/*
+    make a cel to far converter, use float and then make sure the output is a table
+    you should print cel values if far values are 0, 20, ... 300
+*/
+
+// basically the same thing but the other way
+//the eq :  (C × 9/5) + 32 = F
+
+int main(int argc, char const *argv[])
+{
+    float far, cel, lower, upper, step; // we can have var declarations in one line
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    printf("farValue: \t celValue: \n");    // \t indicates a tab (4 spaces) and \n makes  a new line since printf can't make one..
+    printf("--------------------------\n"); // print as a table like the specifications on page 16 of the book
+
+    return 0;
+}
