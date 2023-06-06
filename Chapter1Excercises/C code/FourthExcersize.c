59db7ad46cf37d02adc32b2e70b0508827604b39
Atharve Pandey <pandey.atharve@gmail.com>
2023-06-06 12:34:28 -0400

    added more backbone for others to go off of when they do the fourth excersize

--- Chapter1Excercises/C code/FourthExcersize.c
+++ Chapter1Excercises/C code/FourthExcersize.c
@@ -18,5 +18,11 @@ int main(int argc, char const *argv[])
     printf("farValue: \t celValue: \n");    // \t indicates a tab (4 spaces) and \n makes  a new line since printf can't make one..
     printf("--------------------------\n"); // print as a table like the specifications on page 16 of the book
 
+    // while (/* condition */)
+    // {
+    //     /* code */
+    // }
+    
+
     return 0;
 }
