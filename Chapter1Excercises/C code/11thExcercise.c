c99b958db35c1a2db1d624f19bb6fe5386a65e77
Atharve Pandey <pandey.atharve@gmail.com>
2023-06-07 14:20:52 -0400

    formatted with prettier

--- Chapter1Excercises/C code/11thExcercise.c
+++ Chapter1Excercises/C code/11thExcercise.c
@@ -1,32 +1,34 @@
-//write test cases for the word count program; any inputs that might
-//uncover any bugs?
-#include<stdio.h>
+// write test cases for the word count program; any inputs that might
+// uncover any bugs?
+#include <stdio.h>
 #define IN 1
 #define OUT 0
 
 int main(void)
 {
-    int c, nl, nw, nc, state; 
+    int c, nl, nw, nc, state;
     state = OUT;
-    nl = nw = nc = 0; 
+    nl = nw = nc = 0;
 
-    while ((c = getchar())!=EOF){
+    while ((c = getchar()) != EOF)
+    {
         ++nc;
-        if(c =='\n'){
+        if (c == '\n')
+        {
             ++nl;
         }
-        if(c == ' ' || c == '\t' || c == '\n'){
+        if (c == ' ' || c == '\t' || c == '\n')
+        {
             state = OUT;
         }
-        else if (state == OUT){
+        else if (state == OUT)
+        {
             state = IN;
             ++nw;
         }
-        
     }
 
-    //edge cases for the above program: 
+    // edge cases for the above program:
 
-    //maybe a line with multiple spaces would set state to out, and back to in later on in the code, so multiple spaces might count as one word
-    
+    // maybe a line with multiple spaces would set state to out, and back to in later on in the code, so multiple spaces might count as one word
 }
