3bf5f65193327ce775b6c3ddc69fc66198e46309
Atharve Pandey <pandey.atharve@gmail.com>
2023-06-07 14:16:46 -0400

    added 11th excercise

---
+++ Chapter1Excercises/C code/11thExcercise.c
@@ -0,0 +1,32 @@
+//write test cases for the word count program; any inputs that might
+//uncover any bugs?
+#include<stdio.h>
+#define IN 1
+#define OUT 0
+
+int main(void)
+{
+    int c, nl, nw, nc, state; 
+    state = OUT;
+    nl = nw = nc = 0; 
+
+    while ((c = getchar())!=EOF){
+        ++nc;
+        if(c =='\n'){
+            ++nl;
+        }
+        if(c == ' ' || c == '\t' || c == '\n'){
+            state = OUT;
+        }
+        else if (state == OUT){
+            state = IN;
+            ++nw;
+        }
+        
+    }
+
+    //edge cases for the above program: 
+
+    //maybe a line with multiple spaces would set state to out, and back to in later on in the code, so multiple spaces might count as one word
+    
+}
