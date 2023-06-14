1fcd28b67ceb5824683da217c29b60760af9e0ad
Atharve Pandey <pandey.atharve@gmail.com>
2023-06-14 13:54:14 -0400

    added 2.5 as an excercise for other people

---
+++ Chapter2Excercises/C code/5thExcercise.c
@@ -0,0 +1,3 @@
+//write a function any(s1,s2) which returns the first location in a string  where any character
+//from the string s2 occurs, or -1 if s1 has no characters from s2
+//dont use strpbrk from the standard library 
